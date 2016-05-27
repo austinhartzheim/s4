570e3108c4ff3c01b5ba1380c5ac4aaed2a3c35e
Austin Hartzheim <austin@austinhartzheim.me>
2016-05-27 03:24:58 +0000

    Created Initializer class to create the .s4 directory and guide the user through the configuration process.

--- src/s4.py
+++ src/s4.py
@@ -17,6 +17,35 @@ class BucketAccess(enum.IntEnum):
     Forbidden = 403
 
 
+class Config():
+    def __init__(self):
+        self.config = {
+            'aws': {
+                'credentials': {
+                    'aws_access_key_id': '',
+                    'aws_secret_access_key': ''
+                },
+                'bucket': {
+                    'name': '',
+                    'path': '/'
+                }
+            },
+            'sync': {
+                'root': {
+                    'path': '.'
+                }
+            }
+        }
+
+    def load(self, path):
+        with open(path) as fp:
+            self.config = toml.load(fp)
+
+    def save(self, path):
+        with open(path, 'w') as fp:
+            toml.dump(fp, self.config)
+
+
 class Main():
 
     def __init__(self, args):
@@ -75,6 +104,44 @@ class Main():
                 raise err
 
 
+class Initializer():
+    '''
+    Create the initial s4 directory structure (with the hidden .s4
+    directory, configuration details, etc.
+    '''
+
+    def __init__(self, args):
+        if not os.path.exists(args.path):
+            print('The specified path does not exist.')
+            exit(1)
+        if not os.path.isdir(args.path):
+            print('The specified path is not a directory.')
+            exit(1)
+
+        print('Creating .s4 directory')
+        configdirpath = os.path.join(args.path, '.s4')
+        os.mkdir(configdirpath, mode=0o700)
+
+        print('Creating .s4/config.toml')
+        configfilepath = os.path.join(configdirpath, 'config.toml')
+        config = Config()
+        config.save(configfilepath)
+
+        print('Collecting user details')
+        response = input('What is your "AWS Access Key ID": ')
+        config.config['aws']['credentials']['aws_access_key_id'] = response
+        response = input('What is your "AWS Access Key Secret": ')
+        config.config['aws']['credentials']['aws_secret_access_key'] = response
+        response = input('Name of the sync destination S3 bucket: ')
+        config.config['aws']['bucket']['name'] = response
+        # TODO: perform validation on the relative path
+        response = input('Relative path inside the bucket to sync to: ')
+        config.config['aws']['bucket']['path'] = response
+
+        print('Saving .s4/config.toml')
+        config.save(configfilepath)
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(prog='s4')
@@ -82,9 +149,16 @@ if __name__ == '__main__':
     parser_init = subparsers.add_parser('init')
     parser_add = subparsers.add_parser('add')
 
-    parser_init.add_argument('path', type=str)
+    parser_init.set_defaults(subcommand='init')
+    parser_init.add_argument('path', type=str, nargs='?', default='.')
+
+    parser_add.set_defaults(subcommand='init')
     parser_add.add_argument('file', type=argparse.FileType('r'), nargs='+')
 
     args = parser.parse_args()
 
-    main = Main(args)
+    # Launch main class based on subcommand
+    if args.subcommand == 'init':
+        main = Initializer(args)
+    else:
+        main = Main(args)
